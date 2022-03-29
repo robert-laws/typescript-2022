c7a69ab72c79ada21e03b2b147a3b5591a1914e6
Robert Laws <roblaws@me.com>
2022-03-29 06:49:24 -0500

    add user example file

---
+++ src/user.ts
@@ -0,0 +1,26 @@
+type UserName = string;
+type UserBirthDate = Date | number | string;
+type AddressableUser = Contact & User;
+
+interface User {
+  id: number;
+  name: UserName;
+  birthDate?: UserBirthDate;
+  status?: UserStatus;
+}
+
+interface Address {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+type UserStatus = 'active' | 'inactive';
+
+let myUser: User = {
+  id: 1,
+  name: 'John',
+  birthDate: new Date(1980, 1, 1),
+  status: 'inactive',
+};
